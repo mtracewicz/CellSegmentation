9136a79a5da3f6a588bbf6136692db17d1bb86de
Michał Tracewicz <m.tracewicz@gmail.com>
2020-12-11 19:57:34 +0100

    Refactored split.py

--- image_preprocessing/split.py
+++ image_preprocessing/split.py
@@ -1,27 +1,59 @@
 import os
+import sys
+from typing import Tuple
+
 import numpy as np
 from PIL import Image
+from progress.bar import ChargingBar as cb
 
-class ImageSplitter():
-    it = 0
 
-    def __init__(self,src_directory,out_directory):
-        self._image_width = 1600
-        self._image_height = 1200
-        self._new_image_size = 200
-        self._pocket = 100
-        self._src_directory = src_directory
-        self._out_directory = out_directory
+def split(filename: str, output_directory: str = 'out',
+          output_shape: Tuple[int, int] = (200, 200),
+          pocket: Tuple(int, int) = (100, 100),
+          extension: str = '.png'):
 
-    def split_into_images(self, image_name):
-        img = np.array(Image.open(f'{self._src_directory}/{image_name}'))
-        number_of_moves_horizontally = int(self._image_width/(self._new_image_size-self._pocket)) -1
-        number_of_moves_vertically = int(self._image_height/(self._new_image_size-self._pocket)) - 1
-        for j in range(number_of_moves_vertically):
-            for i in range(number_of_moves_horizontally):
-                horizontal_start = i*(self._new_image_size - self._pocket)
-                vertical_start = j*(self._new_image_size - self._pocket)
-                tmp_img = Image.fromarray(np.copy(img[vertical_start:vertical_start+self._new_image_size,horizontal_start:horizontal_start+self._new_image_size]))
-                tmp_img.save(f"{self._out_directory}/{ImageSplitter.it}_{'0' if j < 10 else ''}{j}_{'0' if i < 10 else ''}{i}.png")
-        ImageSplitter.it+=1
+    image = np.array(Image.open(filename))
+    image_width, image_height = image.shape[:2]
+    vertical_step, horizontal_step = output_shape
+    vertical_pocket, horizontal_pocket = pocket
+    extension = extension if extension.startswith(".") else f".{extension}"
 
+    number_of_moves_horizontally = int(
+        image_width/(image_width-horizontal_pocket)) - 1
+    number_of_moves_vertically = int(
+        image_height/(image_height-vertical_pocket)) - 1
+    for row in range(number_of_moves_vertically):
+        for column in range(number_of_moves_horizontally):
+            horizontal_start = column*(image_width - horizontal_pocket)
+            vertical_start = row*(image_height - vertical_pocket)
+            tmp_img = Image.fromarray(np.copy(
+                image[vertical_start:vertical_start+vertical_step, horizontal_start:horizontal_start+horizontal_step]))
+            new_filename = os.path.join(
+                output_directory, f'{row}_{column}{extension}')
+            tmp_img.save(new_filename)
+
+
+def split_directory(directory_name: str,
+                    output_shape: Tuple[int, int] = (200, 200),
+                    pocket: Tuple(int, int) = (100, 100),
+                    extension: str = '.png'):
+
+    files = os.listdir(dir)
+    with cb('Splitting', max=len(files)) as bar:
+        for file in files:
+            split(os.path.join(dir, file),
+                  f'out_{file}', output_shape, pocket, extension)
+            bar.next()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 2:
+        print('Usage: python split.py input_directory/input_file')
+        exit(1)
+
+    if os.path.isdir(sys.argv[1]):
+        print('Processing directory')
+        split_directory(*sys.argv[1:])
+    else:
+        print('Processing file')
+        split(*sys.argv[1:])

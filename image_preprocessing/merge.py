bedbcefd6bbc57ee137472e5ba29477506c7660e
Michał Tracewicz <m.tracewicz@gmail.com>
2020-12-11 19:37:51 +0100

    Refactored merge.py

--- image_preprocessing/merge.py
+++ image_preprocessing/merge.py
@@ -1,27 +1,45 @@
 import os
+from typing import Tuple
+
 import numpy as np
 from PIL import Image
 
-class ImageMerger():
-    def __init__(self, src_directory, out_directory):
-        self._src_directory = src_directory
-        self._image_names = os.listdir(src_directory)
-        self._image_names.sort()
-        self._out_directory = out_directory
 
-    def merge(self, fileprefix):
-        tmp = np.zeros((1200,1600,3))
-        images = { val for val in self._image_names if val.startswith(fileprefix)}
-        for img in images:
-            split = img.split('_')
-            row = int(split[1])
-            start_row = 200*int(row) - 100*int(row)
-            column = int(split[2].split('.')[0])
-            start_column = 200*int(column) - 100*int(column)
-            end_row = start_row+200
-            end_column = start_column+200
-            tmp[start_row:end_row,start_column:end_column] = np.array(Image.open(f'{self._src_directory}/{img}'))
+def parse_name(filename: str) -> Tuple[int, int]:
+    if os.path.isdir(filename):
+        raise IsADirectoryError()
+    filename = os.path.basename(filename)
+    filename = os.path.splitext(filename)[0]
+    filenames = filename.split('_')
 
-        tmp = tmp.astype('uint8')
-        img = Image.fromarray(tmp)
-        img.save(f'{self._out_directory}/{fileprefix}.png')
+    if len(filename) < 3:
+        raise IndexError('Filename must have format of *_row_column.extension')
+
+    row = int(filenames[-2])
+    column = int(filenames[-1])
+    return (row, column)
+
+
+def merge_files(src_directory: str,
+                output_shape: Tuple[int, int, int] = (1200, 1600, 3),
+                input_shape: Tuple[int, int] = (200, 200),
+                pocket: Tuple[int, int] = (200, 200),
+                output_filename: str = 'out.png'):
+
+    container = np.zeros(output_shape)
+    images = os.listdir(src_directory)
+    vertical_step, horizontal_step = input_shape
+    vertical_pocket, horizontal_pocket = pocket
+
+    for image in images:
+        row, column = parse_name(image)
+        start_row = row * (vertical_step - vertical_pocket)
+        start_column = column * (horizontal_step - horizontal_pocket)
+        end_row = start_row + vertical_step
+        end_column = start_column + horizontal_step
+        container[start_row:end_row, start_column:end_column] = np.array(
+            Image.open(os.path.join(src_directory, image)))
+
+    container = container.astype('uint8')
+    img = Image.fromarray(container)
+    img.save(output_filename)

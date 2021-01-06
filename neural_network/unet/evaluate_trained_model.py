1a4f7d479306341086aa414009119905dae03687
Michał Tracewicz <m.tracewicz@gmail.com>
2021-01-06 22:13:09 +0100

    Added evaluation

--- neural_network/unet/evaluate_trained_model.py
+++ neural_network/unet/evaluate_trained_model.py
@@ -1,6 +1,8 @@
-import tensorflow as tf
 import os
 import sys
+
+import tensorflow as tf
+from neural_network.unet.image_loader import load_images
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -11,5 +13,7 @@ if __name__ == "__main__":
     model = tf.keras.models.load_model(
         os.path.join('trained_models', sys.argv[1]))
     # Loading data
+    x_test = load_images(sys.argv[1])
+    y_test = load_images(sys.argv[2])
 
     model.evaluate(x_test, y_test)

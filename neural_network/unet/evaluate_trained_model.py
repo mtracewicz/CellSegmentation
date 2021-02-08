692a8c80f8403ebe47f1c51186de8161d8b6444b
Michał Tracewicz <m.tracewicz@gmail.com>
2021-02-08 20:39:28 +0100

    Evaluation fixes

--- neural_network/unet/evaluate_trained_model.py
+++ neural_network/unet/evaluate_trained_model.py
@@ -5,15 +5,20 @@ import tensorflow as tf
 from neural_network.unet.image_loader import load_images
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: evaluate_trained_model model_name input_directory output_directory")
+    if len(sys.argv) < 3:
+        print(
+            "Usage: evaluate_trained_model model_name predictions truths [custom_functions]")
         exit()
 
     # Restoring model
+    metric_and_loss_file = sys.argv[4] if len(
+        sys.argv) >= 5 else 'neural_network.unet.dice_coef'
+    exec(f'from {metric_and_loss_file} import custom_metric, custom_loss')
+
     model = tf.keras.models.load_model(
-        os.path.join('trained_models', sys.argv[1]))
+        os.path.join('trained_models', sys.argv[1]), custom_objects={'custom_metric': custom_metric, 'custom_loss': custom_loss})
     # Loading data
-    x_test = load_images(sys.argv[1])
-    y_test = load_images(sys.argv[2])
+    x_test = load_images(sys.argv[2])
+    y_test = load_images(sys.argv[3])
 
     model.evaluate(x_test, y_test)
